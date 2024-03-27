d22eabcffb23c2f7864b56e119acc7e098f3a6c8
cheonjiyun <gunbam715@gmail.com>
2024-03-27 18:53:29 +0900

    contents: 폰프리뷰 추가

---
+++ main.js
@@ -0,0 +1,12 @@
+// const headerDOM = document.getElementsByTagName("header")[0];
+// const toInfoDOM = document.getElementById("toInfo");
+// const toProjectsDOM = document.getElementById("toProjects");
+
+// const infoSectionDOM = document.getElementById("info");
+// const projectSectionDOM = document.getElementById("projects");
+
+// console.log(headerDOM);
+
+// toInfoDOM.addEventListener("click", () => {
+//     window.scrollTo(0, infoSectionDOM.offsetTop - headerDOM.offsetHeight);
+// });
